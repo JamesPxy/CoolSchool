fa215e7f12dff3be00817e447d1664161119fb3c
JamesPxy <1280272327@qq.com>
2017-11-20 11:54:42 +0800

    唤醒毕设
    唤醒毕设，优化试题模块，Bmob后台解冻
    PS：图片上传模块由于Bmob文件服务器地址改变，暂无法上传

--- StudyHelper/libraryoflistview/build/generated/source/r/androidTest/debug/com/handmark/pulltorefresh/library/test/R.java
+++ StudyHelper/libraryoflistview/build/generated/source/r/androidTest/debug/com/handmark/pulltorefresh/library/test/R.java
@@ -127,11 +127,11 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int ptrSubHeaderTextAppearance=0x7f01000b;
     }
     public static final class dimen {
-        public static final int header_footer_left_right_padding=0x7f070000;
-        public static final int header_footer_top_bottom_padding=0x7f070001;
-        public static final int indicator_corner_radius=0x7f070002;
-        public static final int indicator_internal_padding=0x7f070003;
-        public static final int indicator_right_padding=0x7f070004;
+        public static final int header_footer_left_right_padding=0x7f060000;
+        public static final int header_footer_top_bottom_padding=0x7f060001;
+        public static final int indicator_corner_radius=0x7f060002;
+        public static final int indicator_internal_padding=0x7f060003;
+        public static final int indicator_right_padding=0x7f060004;
     }
     public static final class drawable {
         public static final int default_ptr_flip=0x7f020000;
@@ -141,23 +141,23 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int indicator_bg_top=0x7f020004;
     }
     public static final class id {
-        public static final int both=0x7f060003;
-        public static final int disabled=0x7f060000;
-        public static final int fl_inner=0x7f06000c;
-        public static final int flip=0x7f060008;
-        public static final int gridview=0x7f060009;
-        public static final int manualOnly=0x7f060004;
-        public static final int pullDownFromTop=0x7f060005;
-        public static final int pullFromEnd=0x7f060002;
-        public static final int pullFromStart=0x7f060001;
-        public static final int pullUpFromBottom=0x7f060006;
-        public static final int pull_to_refresh_image=0x7f06000d;
-        public static final int pull_to_refresh_progress=0x7f06000e;
-        public static final int pull_to_refresh_sub_text=0x7f060010;
-        public static final int pull_to_refresh_text=0x7f06000f;
-        public static final int rotate=0x7f060007;
-        public static final int scrollview=0x7f06000a;
-        public static final int webview=0x7f06000b;
+        public static final int both=0x7f070003;
+        public static final int disabled=0x7f070004;
+        public static final int fl_inner=0x7f07000c;
+        public static final int flip=0x7f07000a;
+        public static final int gridview=0x7f070000;
+        public static final int manualOnly=0x7f070005;
+        public static final int pullDownFromTop=0x7f070006;
+        public static final int pullFromEnd=0x7f070007;
+        public static final int pullFromStart=0x7f070008;
+        public static final int pullUpFromBottom=0x7f070009;
+        public static final int pull_to_refresh_image=0x7f07000d;
+        public static final int pull_to_refresh_progress=0x7f07000e;
+        public static final int pull_to_refresh_sub_text=0x7f070010;
+        public static final int pull_to_refresh_text=0x7f07000f;
+        public static final int rotate=0x7f07000b;
+        public static final int scrollview=0x7f070001;
+        public static final int webview=0x7f070002;
     }
     public static final class layout {
         public static final int pull_to_refresh_header_horizontal=0x7f030000;
